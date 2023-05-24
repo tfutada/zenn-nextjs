1ce7983d4bc9c3108f76022ef4bfd16e908ceeb5
tfutada <tafutada@gmail.com>
2023-05-24 14:05:41 +0900

    redis in action

--- app/myaction2/page.tsx
+++ app/myaction2/page.tsx
@@ -1,19 +1,29 @@
 import {kv} from "@vercel/kv";
+import {revalidatePath} from "next/cache";
+
+interface Cart {
+    id: string;
+    quantity: number;
+}
+
+const KEY = 'cart:456'
 
 export default async function ActionForm() {
-
-    const cart = await kv.get<{ id: string; quantity: number }[]>(`cart:456`);
+    let cart = await kv.get<Cart[]>(KEY);
 
     // Server Actions. These are executed on the server.
-    async function addItem(formData: FormData) {
+    async function addItem(formData: FormData): Promise<void> {
         'use server'; // This is required for server actions.
-        console.log(formData.get('id'));
+        const id = formData.get('id') as string;
+        const vals: Cart[] = [...(cart ?? []), {id, quantity: 1}]
+        await kv.set(KEY, vals);
+        revalidatePath('/myaction2')
     }
 
     return (
         <>
             <div>
-                {cart?.map((item: any) => (
+                {cart?.map((item: Cart) => (
                     <div key={item.id}>
                         {item.id} : {item.quantity}
                     </div>
