3d3243030b0ce276da5b08098f364c4da79ff237
tfutada <tafutada@gmail.com>
2023-05-25 09:07:52 +0900

    redis in action

--- app/myapi/route.ts
+++ app/myapi/route.ts
@@ -2,6 +2,9 @@ import {kv} from '@vercel/kv';
 import {NextResponse} from 'next/server';
 
 export async function GET() {
-    const user = await kv.hgetall('shopping_cart:123');
-    return NextResponse.json(user);
+
+    await kv.set(`cart:123`, "abc");
+
+    const cart = await kv.get('cart:123');
+    return NextResponse.json(cart);
 }
