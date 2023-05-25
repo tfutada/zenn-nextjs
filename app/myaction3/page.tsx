fb804accb16dd32b160f4ec74dab83c31d3e802d
tfutada <tafutada@gmail.com>
2023-05-25 14:43:14 +0900

    redis in action

--- app/myaction3/page.tsx
+++ app/myaction3/page.tsx
@@ -1,20 +1,20 @@
 'use client';
 
-import {useTransition} from "react";
 import {addItem} from "@/app/myaction3/_myactions";
+import {useTransition} from "react";
 
 export default function ActionForm() {
-    // let [isPending, startTransition] = useTransition();
+    let [isPending, startTransition] = useTransition();
 
     const param1 = '123'
 
-    const handler = async () => {
-        await addItem(param1)
+    const handler = async (p: string) => {
+        await addItem(p)
     }
 
     return (
         <>
-            <button onClick={handler}>
+            <button onClick={() => startTransition(() => handler(param1))}>
                 Add To Cart
             </button>
         </>
