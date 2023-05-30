667c0421d9b305ce12f1a87080e4d8a5f9ef41d9
tfutada <tafutada@gmail.com>
2023-05-30 11:35:29 +0900

    middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,27 @@
 import {NextResponse} from 'next/server';
 import type {NextRequest} from 'next/server';
+import {Ratelimit} from "@upstash/ratelimit";
+import {Redis} from "@upstash/redis";
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+const redis = new Redis({
+    url: process.env.KV_REST_API_URL,
+    token: process.env.KV_REST_API_TOKEN
+});
+
+const ratelimit = new Ratelimit({
+    redis: redis,
+    limiter: Ratelimit.slidingWindow(5, "10 s"),
+});
+
+
+export async function middleware(request: NextRequest) {
     console.log('middleware')
-    return NextResponse.redirect(new URL('/home', request.url));
+    const ip = request.ip ?? "127.0.0.1";
+    const {success, pending, limit, reset, remaining} = await ratelimit.limit(
+        ip
+    );
+    return success
+        ? NextResponse.next()
+        : NextResponse.redirect(new URL("/blocked", request.url));
 }
 
-export const config = {
-    matcher: [
-        /*
-         * Match all request paths except for the ones starting with:
-         * - api (API routes)
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico (favicon file)
-         */
-        '/((?!_next/static|_next/image|favicon.ico).*)',
-    ],
-};
