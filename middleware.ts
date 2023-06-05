4ad89be151650c22c60e8d91a6db5e2abe0ccdaa
tfutada <tafutada@gmail.com>
2023-06-05 11:55:18 +0700

    redis rate limit - scratch sliding window

--- middleware.ts
+++ middleware.ts
@@ -17,8 +17,14 @@ const ratelimit = new Ratelimit({
 export async function middleware(request: NextRequest) {
     const key = request.ip ?? "127.0.0.1";
 
+    // measure elaspsed time
+    const start = Date.now();
+
     // const resp = await ratelimit.limit(key);
     const resp = await rateLimit(key, 10, 10);
+
+    const elapsed = Date.now() - start;
+    console.log(`elapsed: ${elapsed} ms`)
 
     console.log(`success: ${resp.success}, remaining: ${resp.remaining}, reset: ${resp.reset}`)
 
