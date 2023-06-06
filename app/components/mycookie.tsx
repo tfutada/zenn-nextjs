066b44e75bcbc5182b74ef32a37b135d0b174cba
tfutada <tafutada@gmail.com>
2023-06-06 10:36:08 +0700

    cookie

--- app/components/mycookie.tsx
+++ app/components/mycookie.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import {useEffect} from "react";
-import { useRouter } from 'next/navigation';
+import {useRouter} from 'next/navigation';
 
 
 export default function SessionCookie() {
