49cefa61b87f7d246a439071da46216b203b16c7
tfutada <tafutada@gmail.com>
2023-06-06 12:05:03 +0700

    cookie

--- app/api-cookie/route.ts
+++ app/api-cookie/route.ts
@@ -1,13 +1,14 @@
 import {cookies} from 'next/headers';
 import {v4} from 'uuid';
 
+const MY_TOKEN = 'mytoken';
 export async function GET(request: Request) {
     const cookieStore = cookies();
-    const token = cookieStore.get('token');
+    const token = cookieStore.get(MY_TOKEN);
 
     const headers: { [key: string]: string } = {}
     if (!token) {
-        headers['Set-Cookie'] = `mytoken=${v4()}; path=/; HttpOnly; SameSite=Strict`;
+        headers['Set-Cookie'] = `${MY_TOKEN}=${v4()}; path=/; HttpOnly; SameSite=Strict`;
     }
 
     return new Response('Hello, Next.js!', {
--- app/mysession/page.tsx
+++ app/mysession/page.tsx
@@ -1,13 +1,19 @@
 import dynamic from 'next/dynamic';
+import {cookies} from 'next/headers';
 
 const SessionCookie = dynamic(() => import('@/app/components/mycookie'), {ssr: false});
+const MY_TOKEN = 'mytoken';
 
 
 export default function MySession() {
+    const cookieStore = cookies();
+    const token = cookieStore.get(MY_TOKEN);
+
+    console.log('token', token)
 
     return (
         <div>
-            <SessionCookie/>
+            {!token && <SessionCookie/>}
         </div>
     )
 }
