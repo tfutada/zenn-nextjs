bcfc15abf879037781bda612c129ddee0e1bc544
tfutada <tafutada@gmail.com>
2023-06-18 13:42:42 +0900

    use pnpm instead of yarn

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,44 @@
-import {withAuth} from "next-auth/middleware"
+import {NextResponse, NextRequest} from 'next/server';
+import {match} from '@formatjs/intl-localematcher';
+import Negotiator from 'negotiator';
 
-export default withAuth(
-    function middleware(req) {
-        // callbacks.authorizedがtrueの場合のみ進入できる
-        console.log('in middleware: ', req.nextauth.token)
-    },
-    {
-        callbacks: {
-            // 認可に関する処理。ロールが `admin` ならOK
-            // authorized: ({token}) => {
-            //     console.log('in authorized: ', token)
-            //     return token?.role === "admin"
-            // },
-        },
+let locales = ['en', 'es'];
+let defaultLocale = 'en';
+
+function getLocale(request: NextRequest) {
+    // Retrieving languages from request headers
+    let headers = {'accept-language': 'en-US,en;q=0.5'};
+    let languages = new Negotiator({headers}).languages();
+    let defaultLocale = 'en';
+    let locale = match(languages, locales, defaultLocale);
+    console.log(locale);
+    return locale || defaultLocale;
+}
+
+export function middleware(request: NextRequest) {
+    // Check if there is any supported locale in the pathname
+    const pathname = request.nextUrl.pathname;
+    const pathnameIsMissingLocale = locales.every(
+        (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+    );
+
+    console.log(pathnameIsMissingLocale);
+    // Redirect if there is no locale
+    if (pathnameIsMissingLocale) {
+        const locale = getLocale(request);
+
+        // Redirect to the URL with the determined locale
+        return NextResponse.redirect(
+            new URL(`/${locale}/${pathname}`, request.url)
+        );
     }
-)
+}
 
 export const config = {
-    matcher: ["/((?!register|api|login).*)"]
+    matcher: [
+        // Skip all internal paths (_next)
+        '/((?!_next).*)',
+        // Optional: only run on root (/) URL
+        // '/'
+    ],
 };
