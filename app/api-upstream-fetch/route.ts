6cc38fb50229d22c475fe1bda868baa5c9074ad0
tfutada <tafutada@gmail.com>
2023-06-19 21:59:08 +0900

    geo

--- app/api-upstream-fetch/route.ts
+++ app/api-upstream-fetch/route.ts
@@ -4,37 +4,33 @@ import {unwrap} from "@/app/common";
 const upstream_url = unwrap(process.env.UPSTREAM_URL)
 
 export async function GET(request: Request) {
-    try {
-        // fetch an upstream api
-        const res = await fetch(`${upstream_url}/set_cookie`);
 
-        // log the headers
-        for (let [key, value] of res.headers.entries()) {
-            console.log(`${key}: ${value}`);
-        }
+    // fetch an upstream api
+    const res = await fetch(`${upstream_url}/set_cookie`, {
+        next: {revalidate: 60}
+    });
 
-        const json = await res.json();
-        console.log(json);
-
-        const headers: { [key: string]: string } = {}
-        // my_cookie=my_value; Path=/; Max-Age=1687226886; HttpOnly
-        const set_cookie = res.headers.get('set-cookie')
-        console.log(set_cookie)
-        if (set_cookie) {
-            headers['set-cookie'] = set_cookie
-        } else {
-            console.log('!!! no set-cookie')
-        }
-
-
-        return new Response('Hello, Next.js!', {
-            status: 200,
-            headers
-        });
-    } catch (error) {
-        console.error('Error fetching data from upstream API:', error);
-        return new Response('Error fetching data from upstream API', {
-            status: 500,
-        });
+    // log the headers
+    for (let [key, value] of res.headers.entries()) {
+        console.log(`${key}: ${value}`);
     }
+
+    const json = await res.json();
+    console.log(json);
+
+    const headers: { [key: string]: string } = {}
+    // my_cookie=my_value; Path=/; Max-Age=1687226886; HttpOnly
+    const set_cookie = res.headers.get('set-cookie')
+    console.log(set_cookie)
+    if (set_cookie) {
+        headers['set-cookie'] = set_cookie
+    } else {
+        console.log('!!! no set-cookie')
+    }
+
+    return new Response('Hello, Next.js!', {
+        status: 200,
+        headers
+    });
+
 }
